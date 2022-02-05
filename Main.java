15c11e7808bbfd46483bc83026bbced2fdbcef68
David Carrillo <dav.t02@hotmail.com>
2022-02-05 15:57:04 -0600

    Agregando lectura del libro de Elon Musk a Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
 	public static void main(String args[]){
 		System.out.println("Hello Vim, Hello Git");
 		Libro libro = new Libro();
+		libro.setTitle("Elon Musk: El empresario que anticipa el futuro");
 		System.out.println(libro.leerLibro());
 	}
 }
